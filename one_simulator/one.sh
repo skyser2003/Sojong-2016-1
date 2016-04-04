bc305db3087efcf6c692b3ad1042d942d036fafc
Oh Jinkyun <tintypemolly@gmail.com>
2016-04-04 19:39:12 +0900

    added jar to one.sh

--- one_simulator/one.sh
+++ one_simulator/one.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar:lib/json-simple-1.1.1.jar core.DTNSim $*
