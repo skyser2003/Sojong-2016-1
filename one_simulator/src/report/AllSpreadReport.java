257c6f1505246e60e6311dffa3469d930ba1750d
Seung Ho Jang <shjang1992@gmail.com>
2016-04-04 19:25:44 +0900

    AllSpreadReport writes in JSON format

--- one_simulator/src/report/AllSpreadReport.java
+++ one_simulator/src/report/AllSpreadReport.java
@@ -3,42 +3,51 @@ package report;
 import java.util.HashSet;
 import java.util.List;
 
+import org.json.simple.*;
+
 import core.DTNHost;
 import core.Settings;
 import core.SimClock;
 import core.UpdateListener;
 
 public class AllSpreadReport extends Report implements UpdateListener {
-	private boolean written = false;
+    private boolean written = false;
 
-	public AllSpreadReport() {
-		init();
-	}
+    public AllSpreadReport() {
+        init();
+    }
 
-	@Override
-	public void updated(List<DTNHost> hosts) {
-		if (isWarmup() == true || written == true) {
-			return;
-		}
+    @Override
+    public void updated(List<DTNHost> hosts) {
+        if (isWarmup() == true || written == true) {
+            return;
+        }
 
-		boolean allSpread = true;
+        boolean allSpread = true;
 
-		int count = 0;
+        int count = 0;
 
-		for (DTNHost host : hosts) {
-			if (host.getMessageCollection().size() == 0) {
-				allSpread = false;
-			} else {
-				++count;
-			}
-		}
+        for (DTNHost host : hosts) {
+            if (host.getMessageCollection().size() == 0) {
+                allSpread = false;
+            } else {
+                ++count;
+            }
+        }
 
-		if (allSpread == true) {
-			written = true;
-			write("All spread! time : " + SimClock.getTime());
-		} else {
-			write("time : " + SimClock.getTime() + ", Current spread : " + count + ", total count : " + hosts.size());
-		}
-	}
+        if (allSpread == true) {
+            written = true;
+        }
 
+        writeJSON(SimClock.getTime(), count, hosts.size());
+    }
+
+    private void writeJSON(double time, int current, int max) {
+        JSONObject obj = new JSONObject();
+        obj.put("time", time);
+        obj.put("current_spread", current);
+        obj.put("max_count", max);
+
+        write(obj.toJSONString());
+    }
 }
