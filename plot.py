dedc4f915dd8e8ed9b8cda2a6c91103369d5c5a1
Gyusun Yeom <omniavinco@gmail.com>
2016-04-28 00:22:22 +0900

    clean after each trial plot + stastics plot

--- plot.py
+++ plot.py
@@ -5,17 +5,41 @@ import sys
 import matplotlib.pyplot as pyplot
 
 
-def plot_report(input_filename):
-    with open(input_filename) as f:
+def read_report(filename):
+    with open(filename) as f:
         points = list()
         for line in f:
             point = json.loads(line)
             points.append(point)
-        times = [point['time'] for point in points]
-        current_spread = [point['current_spread'] for point in points]
-        pyplot.plot(times, current_spread)
-        result_filename = os.path.splitext(input_filename)[0] + '.png'
-        pyplot.savefig(result_filename)
+
+        return points
+
+
+def plot_report(points, output_filename):
+    times = [point['time'] for point in points]
+    current_spread = [point['current_spread'] for point in points]
+    p = pyplot.plot(times, current_spread)
+    if output_filename:
+        pyplot.savefig(output_filename)
+        pyplot.cla()
+    return p
+
+
+def plot_stastics(data, output_filename):
+    i = 0
+    plts = []
+    names = []
+    for k, v in data.items():
+        mean = v['mean']
+        err_min = mean - v['min']
+        err_max = v['max'] - mean
+        p = pyplot.errorbar([i / len(data)], [mean],
+                            yerr=[[err_min], [err_max]])
+        plts.append(p)
+        names.append(k)
+        i = i + 1
+    pyplot.legend(plts, names)
+    pyplot.savefig(output_filename)
 
 
 def main():
--- run_all.py
+++ run_all.py
@@ -6,6 +6,7 @@ import os.path
 import glob
 import shutil
 import platform
+import json
 from multiprocessing import Pool, cpu_count
 
 cpu_count = cpu_count()
@@ -49,23 +50,22 @@ def compile():
         shutil.copytree('src/gui/buttonGraphics', 'target/gui/buttonGraphics')
 
 
-def write_plot(index, sim_name):
-    plot.plot_report(
-        os.path.join('reports',
-                     '{0}_AllSpreadReport_{1}.txt'.format(sim_name, index)))
+def read_report(index, sim_name):
+    path = os.path.join('reports',
+                        '{0}_AllSpreadReport_{1}.txt'.format(sim_name, index))
+    return plot.read_report(path), path
+
+
+def write_plot(data, path):
+    plot.plot_report(data, path)
 
 
 def main():
     os.chdir('one_simulator')
 
-    if os.path.exists('reports'):
-        shutil.rmtree('reports')
-
-    os.makedirs('reports')
-
-    compile()
     parser = argparse.ArgumentParser()
     parser.add_argument('-np', dest='plot', action="store_false")
+    parser.add_argument('-r', dest='report', action='store_true')
     parser.add_argument('count', type=int)
     parser.add_argument('settings', type=str, nargs='*',
                         default=['setcover_settings.txt',
@@ -78,19 +78,48 @@ def main():
     parser.add_argument('-t', dest='thread', type=int, default=cpu_count)
     args = parser.parse_args()
 
-    pool = Pool(args.thread)
-    for i in range(0, args.count):
-        for setting in args.settings:
-            pool.apply_async(run, (i + 1, args.count, setting))
+    if not args.report:
+        compile()
+        if os.path.exists('reports'):
+            shutil.rmtree('reports')
 
-    pool.close()
-    pool.join()
+        os.makedirs('reports')
+
+        pool = Pool(args.thread)
+        for i in range(0, args.count):
+            for setting in args.settings:
+                pool.apply_async(run, (i + 1, args.count, setting))
+
+        pool.close()
+        pool.join()
 
     if args.plot:
+        stastics = {}
         for setting in args.settings:
+            all_data = []
             for i in range(0, args.count):
                 sim_name = get_sim_name(setting)
-                write_plot(i + 1, sim_name)
+                report_data, path = read_report(i + 1, sim_name)
+                output = os.path.splitext(path)[0] + '.png'
+                write_plot(report_data, output)
+                for data in report_data:
+                    if data['current_spread'] / float(data['max_count']) > 0.8:
+                        all_data.append(data['time'])
+                        break
+            mean = sum(all_data) / len(all_data)
+            variance = sum(map(lambda x: x ** 2,
+                               all_data)) / len(all_data) - mean ** 2
+            min_val = min(all_data)
+            max_val = max(all_data)
+            stastics[setting] = {
+                'mean': mean,
+                'variance': variance,
+                'min': min_val,
+                'max': max_val
+            }
+        plot.plot_stastics(stastics, os.path.join('reports', 'stastics.png'))
+        with open(os.path.join('reports', ' stastics.txt'), 'w') as f:
+            json.dump(stastics, f)
 
 
 if __name__ == "__main__":
